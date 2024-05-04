27fbaa5c0f785fadaea3fec9ea7ee9f1b967d78f
Hari Vikinesh <harivikinesh@gmail.com>
2024-05-04 23:06:33 +0530

    feat: i2c modified for mpu6050
    Signed-off-by: Hari Vikinesh <harivikinesh@gmail.com>

--- projects/i2c_testing/code/I2C_Controller.v
+++ projects/i2c_testing/code/I2C_Controller.v
@@ -17,26 +17,6 @@ Example Call  :	module I2C_Controller(  .clk(clk),
                                     );
 */
 
-
-
-// assuming PIN-12(ALT-ADDRESS) of the ADXL345 IS GROUNDED = 0x53 is the address of the adxl345 sensor on-board
-// CS of core is connected to CS' (pin 7) OF ADXL345 i.e I2C IS ENABLED ON THE CHIP
-// 100hz default speed of communication on ADXL345
-// PWR-CTL Regester is 0x00 by default which puts the sensor in standby mode , thus measurement bit need to be set.
-// we are performing two write operation and two read operation.
-// 1st write to set the necessary bits in resolution regester.
-// 2nd write to set the measurement bit in the PWR-CTL regester so that sensor initiates its measuremt.
-// 2 read operation on 0x32 address to obtain x axis data.
-
-// 0x2D POWER_CTL   =8'b00001000  (measurement bit high)
-// 0x31 DATA_FORMAT =8'b00000000  ( justify bit=0 LSB mode with  +/- 2g 10 bit resolution)
-
-
-
-// one is to program again and again
-// one is to directly start the oeration of adxl
-
-
 module I2C_Controller(
 
 input  clk,                   // 50MHz
@@ -53,12 +33,9 @@ output reg [7:0] reg_data		// output to core
 //////////////////////////VARIABLES AND PARAMETERS////////////////////
 
 localparam	IDLE            = 3'b000;
-localparam	SET_RESOLUTION  = 3'b001;
-localparam	WAIT_1_CYCLE_1  = 3'b010;
-localparam	START_OPERATION = 3'b011;
-localparam	WAIT_1_CYCLE_2  = 3'b100;
-localparam	READ_DATA       = 3'b101;
-localparam	WAIT_1_CYCLE_3  = 3'b111;
+localparam	START_OPERATION = 3'b001;
+localparam	WAIT_ACK        = 3'b010;
+localparam	READ_DATA       = 3'b011;
 
 
 reg [2:0] nst =IDLE;        // tracks the next state of the programme
@@ -82,28 +59,28 @@ always@(posedge clk,negedge rst) begin
             else nst<=IDLE;
             end
 
-		START_OPERATION:                           // sets the POWER_CTL regester
+		START_OPERATION: // sets the POWER_CTL regester
             begin
                 if (!core_busy)
                 begin
                     rw        <=0;                  // write operation
-                    slave_addr<=7'h1D;
-                    reg_addr  <=8'h2D;              // reg address
-                    reg_data  <=8'h08;              // sets the measurement bit
+                    slave_addr<=7'h68;              // slave address of mpu6050
+                    reg_addr  <=8'h6B;              // PWR_MGT1 reg address
+                    reg_data  <=8'h01;              // sets the clock for x reference
                     data_valid<=1'b1;               // send the data
-                    nst<=WAIT_1_CYCLE_2;
+                    nst<=WAIT_ACK;
                 end
                 else nst<=START_OPERATION;
             end
 
-		WAIT_1_CYCLE_2:
+		WAIT_ACK:
             begin
 
             data_valid<=1'b0;
                 if (!core_busy)
                     nst<=READ_DATA;
                 else
-                    nst<=WAIT_1_CYCLE_2;
+                    nst<=WAIT_ACK;
             end
 
 		READ_DATA:
@@ -111,8 +88,8 @@ always@(posedge clk,negedge rst) begin
                 if (!core_busy)
                     begin
                     rw        <=1;                  // read operation
-                    slave_addr<=7'h1D;
-                    reg_addr  <=8'h34;              // y-axis data regester in ADXL345 , if we make subsequent read operation, it will give data present at next address of reg
+                    slave_addr<=7'h68;
+                    reg_addr  <=8'h43;              // GYRO X-axis data register in MPU6050, next address of reg
                     reg_data  <=8'h00;
                     data_valid<=1'b1;               // send the data
                     end
